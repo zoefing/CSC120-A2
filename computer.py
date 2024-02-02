f156f978ccec02831ef458b4f0505833967dca93
Zoe Fingleton <141530755+zoefing@users.noreply.github.com>
2024-02-01 22:25:57 -0500

    first attempt save

--- computer.py
+++ computer.py
@@ -1,10 +1,43 @@
 class Computer:
 
-    # What attributes will it need?
+    # attributes - information about a specific computer
+    
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # constructors
+    def __str__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
 
-    # What methods will you need?
+    # methods
+    
+    def update_OS(self, new_operating_system):
+        """ updates the computer's operating system
+
+        args:
+            new_operating_system (str): new operating system to be installed
+        """
+        self.operating_system == new_operating_system
+        
+    def refurbish(self, new_processor_type, new_hard_drive_capacity, new_memory):
+        """ updates the computer's processor type, hard drive capacity, and memory
+
+        args:
+            processor_type (str): current processor type pre-refurbishment
+            hard_drive_capacity (int): current hard drive capacity pre-refurbishment
+            memory (int): current memory pre-refurbishment
+        """
+        self.processor_type == new_processor_type
+        self.hard_drive_capacity == new_hard_drive_capacity
+        self.memory == new_memory
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,50 @@
 class ResaleShop:
 
-    # What attributes will it need?
+    # attributes
+    
+    inventory: dict
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # constructors
 
-    # What methods will you need?
+    def __init__(self, inventory):
+        self.inventory = inventory
+
+    # methods
+    
+    # not given a limited amount of funds, so no subtraction of shop money
+    def buy_computer(self, new_computer):
+        """ adds a new computer to the shop inventory
+
+        args:
+            new_computer (str): computer being bought
+       
+        """
+        # add computer to inventory dictionary
+        self.inventory.append(new_computer)
+    
+    def sell_computer(self, computer):
+        """ removes a computer from the shop inventory
+
+        args:
+            computer (str): computer being sold
+        """
+        # if computer in inventory, remove computer from inventory
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+        # error message
+        else:
+            print("Computer not in inventory")
+    
+    def update_computer_price(self, computer, new_price):
+        """ updates a computer's price in the shop inventory
+
+        Args:
+            computer (_type_): _description_
+            new_price (_type_): _description_
+        """
+        # if computer in inventory, update computer price
+        if computer in self.inventory:
+            self.inventory["price"] = new_price
+        # error message
+        else:
+            print("Computer not in inventory")
